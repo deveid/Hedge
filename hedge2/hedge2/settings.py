54070999f8313a6d59f168fa83d4f52f633acb53
deveid <deveidtolu18@gmail.com>
2019-07-03 07:29:21 +0000

    Update settings.py

--- hedge2/hedge2/settings.py
+++ hedge2/hedge2/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
 
 ]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
